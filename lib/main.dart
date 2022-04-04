70b006aa95184592886c0cdca05491bdf357c283
🐼🐼 <camchin1@msn.cn>
2022-04-04 19:44:20 +0800

    熊凯：update at 2022-04-04 19:44:20

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rabbit/rabbit_page.dart';
+
+import 'rabbit/rabbit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Rabbities',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
---
+++ lib/rabbit/rabbit.dart
@@ -0,0 +1,4 @@
+export 'path_ext.dart';
+export 'rabbit_page.dart';
+export 'rabbit_painter.dart';
+export 'rect_ext.dart';
