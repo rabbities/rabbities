8acfd41d2ed2bed30d5d66538f70550f77c12a94
🐼🐼 <camchin1@msn.cn>
2022-06-17 13:39:10 +0800

    熊凯：upgrade at 2022-06-17 13:39:09

--- lib/rabbit/rabbit_page.dart
+++ lib/rabbit/rabbit_page.dart
@@ -27,9 +27,12 @@ class _RabbitPageState extends State<RabbitPage> with TickerProviderStateMixin {
   void initAnimation() {
     for (var key in animationDurationMap.keys) {
       if (key == "border") {
-        animationControllerMap[key] = AnimationController(vsync: this, upperBound: 15.0)..duration = animationDurationMap[key];
+        animationControllerMap[key] =
+            AnimationController(vsync: this, upperBound: 15.0)
+              ..duration = animationDurationMap[key];
       } else {
-        animationControllerMap[key] = AnimationController(vsync: this)..duration = animationDurationMap[key];
+        animationControllerMap[key] = AnimationController(vsync: this)
+          ..duration = animationDurationMap[key];
       }
     }
 
@@ -57,7 +60,10 @@ class _RabbitPageState extends State<RabbitPage> with TickerProviderStateMixin {
         color: Colors.white,
         child: Center(
           child: CustomPaint(
-            painter: RabbitPainter(controller: Listenable.merge(animationControllerMap.values.toList()), animationMap: animationControllerMap),
+            painter: RabbitPainter(
+                controller:
+                    Listenable.merge(animationControllerMap.values.toList()),
+                animationMap: animationControllerMap),
             size: Size(0.8.sw, 1.sw),
           ),
         ),
@@ -66,8 +72,8 @@ class _RabbitPageState extends State<RabbitPage> with TickerProviderStateMixin {
   }
 
   void initScreenUtil(BuildContext context) {
-    ScreenUtil.init(context, designSize: const Size(360, 690), minTextAdapt: true, orientation: Orientation.portrait);
-    // ScreenUtil.init(  designSize: const Size(375, 812) );
+    ScreenUtil.init(context,
+        designSize: const Size(360, 690), minTextAdapt: true);
   }
 
   void exec(AnimationController current, AnimationController next) {
